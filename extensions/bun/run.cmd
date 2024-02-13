1b6ffccf9864eb6b9f38da13b8454898f89974cc
Harald Schneider <h_schneider@marketmix.com>
2024-02-13 23:55:03 +0100

    run.cmd aktualisieren

--- extensions/bun/run.cmd
+++ extensions/bun/run.cmd
@@ -3,5 +3,5 @@ set BUN_INSTALL="%1\extensions\bun\_runtime"
 if exist "%1\extensions\bun\main.js" (
     %BUN_INSTALL%\bin\bun run --inspect "%1\extensions\bun\main.js" %2=%3 %4=%5 %5=%6
 ) else (
-    %BUN_INSTALL%\bin\bun run --inspect "%1\extensions\bun\main-app.exe" %2=%3 %4=%5 %5=%6
+    "%1\extensions\bun\main-app.exe" %2=%3 %4=%5 %5=%6
 )
