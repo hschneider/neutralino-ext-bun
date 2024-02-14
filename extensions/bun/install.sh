8c443205b627ab4f2fce4873b2c87b4276c2e1ca
hschneider <h_schneider@marketmix.com>
2024-02-14 22:12:16 +0100

    Update

--- extensions/bun/install.sh
+++ extensions/bun/install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-echo "Check if Bun runtime exist on the machine..."
+echo "Checking if the Bun runtime exists ..."
 if [ -n "$BUN_INSTALL" ]; then
     if [ -d "extensions" ]; then
       cd ./extensions/bun
     fi
 
-    echo "Copy Bun binary to the extension folder..."
+    echo "Copying Bun binary to the extension folder..."
     mkdir -p _runtime/bin
     cp $BUN_INSTALL/bin/bun _runtime/bin
 
     if [[ $(uname) == "Darwin" ]]; then
-      echo "Remember to sign the extensions/_runtime/bin/bun binary before building your app bundle."
+      echo "Remember to sign the extensions/_runtime/bin/bun binary before building your app-bundle."
     fi
 
     echo "✔️ DONE 😊"
