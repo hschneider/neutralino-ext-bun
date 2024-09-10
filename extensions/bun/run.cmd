f87146ecfa8019a3b2c52ab8d9ae5e4fdb781872
hschneider <h_schneider@marketmix.com>
2024-09-10 10:11:12 +0200

    Fixed path with spaces

--- extensions/bun/run.cmd
+++ extensions/bun/run.cmd
@@ -1,8 +1,8 @@
 @echo off
-set BUN_INSTALL=%1\extensions\bun\_runtime
+set BUN_INSTALL=%~1\extensions\bun\_runtime
 
-if exist "%1\extensions\bun\main.js" (
-    "%BUN_INSTALL%\bin\bun.exe" run "%1\extensions\bun\main.js"
+if exist "%~1\extensions\bun\main.js" (
+    "%BUN_INSTALL%\bin\bun.exe" run "%~1\extensions\bun\main.js"
 ) else (
-    "%1\extensions\bun\main-app.exe"
+    "%~1\extensions\bun\main-app.exe"
 )
